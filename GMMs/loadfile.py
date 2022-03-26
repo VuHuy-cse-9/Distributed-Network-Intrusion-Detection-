3bf973bbd03cc5dff377bc4103a0745aaa06a43d
toan lam <lamtoan557@gmail.com>
2022-03-27 06:56:47 +0700

    Category data to 0 1 data

--- GMMs/loadfile.py
+++ GMMs/loadfile.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # D:\Tryhard\212-Semester\Parallel Computing\Assignment\Distributed-Network-Intrusion-Detection-\dataset\kddcup1999_10percent.csv
-df = pd.read_csv (r'dataset\kddcup1999_10percent.csv')
-
-npArray = df.to_numpy()
-print(npArray)
+def loadFile(csvPathFile):
+    df = pd.read_csv (r(csvPathFile)) # load file using pandas
+    npArray = df.to_numpy() #pandas to numpy
+    return npArray
